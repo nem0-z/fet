ec5203c25d593f86b09b295c7ed64e264b4909fb
Zlatan Radovanovic <annoyingzlatan@gmail.com>
2020-03-24 09:26:00 +0100

    have no idea

--- arch/pracc/hw1/6.s
+++ arch/pracc/hw1/6.s
@@ -6,16 +6,9 @@ b: .hword 4
 .globl main
 .globl exchange
 
-
-exchange:
-lh $s0,($a0)
-sh $a1,($a0)
-addu $v0,$0,$t0
-jr $ra
-
 main:
-addiu $sp,$sp,-20
-sw $ra,16($sp)
+addiu $sp,$sp,-4
+sw $ra,($sp)
 
 la $a0,a
 la $a1,b
@@ -23,8 +16,13 @@ lh $a1,($a1)
 jal exchange
 
 end: # bpoint for debugging
-lw $ra,16($sp)
-addiu $sp,$sp,20
+lw $ra,($sp)
+addiu $sp,$sp,4
 li $v0,0
 jr $ra
 
+exchange:
+lh $t0,($a0)
+sh $a1,($a0)
+addu $v0,$0,$t0
+jr $ra
