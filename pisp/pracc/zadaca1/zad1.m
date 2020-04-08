48c2658f03f77cc5d5cb78e97aba6da5ed209e48
Zlatan Radovanovic <annoyingzlatan@gmail.com>
2020-04-08 22:20:35 +0200

    pisp dz

--- pisp/pracc/zadaca1/zad1.m
+++ pisp/pracc/zadaca1/zad1.m
@@ -4,24 +4,21 @@ clc
 
 E = [1 3 3; 1 2 6;8 3 1]
 
-%a
-
+% a
+% 
 % A = [2:5;9:-2:3;2.^[2:5];3.^[3:-1:0]];
-
-%b
+% 
+% b
 % C = fliplr(diag(diag(fliplr(B))));
-
-%c
+% 
+% c
 % X = prod(diag(fliplr(D),1))
-
-%d
-X = find(mod(E,2) == 0);
-Y = E(find(mod(find(mod(E,2) == 0),2)));
-
-
-
-%e
-%Z = diag(rot90(E,-1).*rot90(E))
+% 
+% d
+% Y = E(1:2:end);Y=Y(find(mod(E(1:2:end),2) == 0));
+% 
+% e
+% Z = diag(rot90(E,-1).*rot90(E))
 
 
 
--- pisp/pracc/zadaca1/zad3.m
+++ pisp/pracc/zadaca1/zad3.m
@@ -6,14 +6,18 @@ x = [1 2 3 4];
 [A,B,C] = z3(x);
 
 function [A,B,C] = z3(x)
-    
+     n = length(x);
+     
+    if n<4
+        error('Duzina vektora mora biti minimalno 4');
+    end
     if nargin~=1
         error('Samo jedan ulazni arg')
     end
     if nargout ~=3
         error('Samo 3 izlazna arg')
     end
-    n = length(x);
+   
     A = zeros(n,3*n);
     A(1:n,1:n) = diag(x);
     A(:,[1 n n+1]) = [x' x' x'];
@@ -24,7 +28,7 @@ function [A,B,C] = z3(x)
     
     B = ones(2*n-1);
     for i = 1:n
-        B([i 2*n - i],i:2*n-i) = i;
+        B(i: 2*n-i,i:2*n-i) = i;
     end
     
     
